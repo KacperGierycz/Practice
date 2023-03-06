1df58018d69127c969ebf3b240b2c72b70e909f1
Kacper <kacpergierycz@gmail.com>
2023-03-06 15:14:19 +0100

    Generics methods WildCards and TypeErasure

---
+++ src/main/java/generics/BoxDemo.java
@@ -0,0 +1,38 @@
+package generics;
+
+public class BoxDemo {
+
+    public static <U> void addBox(U u, java.util.List<Box<U>> boxes) {
+        Box<U> box = new Box<>();
+        box.set(u);
+        boxes.add(box);
+    }
+
+    public static <U> void outputBoxes(java.util.List<Box<U>> boxes) {
+        int counter = 0;
+        for (Box<U> box: boxes) {
+            U boxContents = box.get();
+            System.out.println(
+                    "Box #" + counter + " contains [" +
+                            boxContents.toString() + "]");
+            counter++;
+        }
+    }
+
+    public static void main(String[] args) {
+        java.util.ArrayList<Box<Integer>> listOfIntegerBoxes =
+                new java.util.ArrayList<>();
+        BoxDemo.<Integer>addBox(Integer.valueOf(10), listOfIntegerBoxes);
+        BoxDemo.addBox(Integer.valueOf(20), listOfIntegerBoxes);
+        BoxDemo.addBox(Integer.valueOf(30), listOfIntegerBoxes);
+        BoxDemo.outputBoxes(listOfIntegerBoxes);
+
+        Box<Number> numberBox = new Box<>();
+        numberBox.set(new Integer(12));
+        numberBox.set(new Double(2.2));
+        Box[] boxes = new Box[5];
+
+
+
+    }
+}
