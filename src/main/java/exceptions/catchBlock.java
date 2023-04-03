d75c1279bf87ffffa872da5ac1eee0337648c025
Kacper <kacpergierycz@gmail.com>
2023-04-03 13:35:05 +0200

    Exceptions Theory try catch finally and try with resources

---
+++ src/main/java/exceptions/catchBlock.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+public class catchBlock {
+
+//    try {
+//
+//    } catch (IndexOutOfBoundsException e) {
+//        System.err.println("IndexOutOfBoundsException: " + e.getMessage());
+//    } catch (IOException e) {
+//        System.err.println("Caught IOException: " + e.getMessage());
+//    }
+
+//    catch (IOException|SQLException ex) {
+//        logger.log(ex);
+//        throw ex;
+//    }
+}
