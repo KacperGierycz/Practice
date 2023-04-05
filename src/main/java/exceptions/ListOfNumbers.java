d0632c6213fad7a3c0e457899dfe8db326e9d44d
Kacper <kacpergierycz@gmail.com>
2023-04-05 14:20:54 +0200

    Exceptions exercise

--- src/main/java/exceptions/ListOfNumbers.java
+++ src/main/java/exceptions/ListOfNumbers.java
@@ -3,31 +3,80 @@ package exceptions;
 import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class ListOfNumbers {
-
     private List<Integer> list;
     private static final int SIZE = 10;
 
     public ListOfNumbers () {
         list = new ArrayList<Integer>(SIZE);
-        for (int i = 0; i < SIZE; i++) {
-            list.add(i);
+        for (int i = 0; i < SIZE; i++)
+            list.add(new Integer(i));
+    }
+    public void writeList() {
+        PrintWriter out = null;
+
+        try {
+            System.out.println("Entering try statement");
+            out = new PrintWriter(new FileWriter("OutFile.txt"));
+
+            for (int i = 0; i < SIZE; i++)
+                out.println("Value at: " + i + " = " + list.get(i));
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Caught IndexOutOfBoundsException: " +
+                    e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Caught IOException: " + e.getMessage());
+        } finally {
+            if (out != null) {
+                System.out.println("Closing PrintWriter");
+                out.close();
+            } else {
+                System.out.println("PrintWriter not open");
+            }
         }
     }
 
-    public void writeList() {
-        PrintWriter out = null;
+    public void readList(String fileName) {
+        String line = null;
         try {
-            System.out.println("Entered try statement");
- //           FileWriter f = new FileWriter("OutFile.txt");
-  //          out = new PrintWriter(f);
-            for (int i = 0; i < SIZE; i++) {
-                out.println("Value at: " + i + " = " + list.get(i));
+            RandomAccessFile raf = new RandomAccessFile(fileName, "r");
+            while ((line = raf.readLine()) != null) {
+                Integer i = new Integer(Integer.parseInt(line));
+                System.out.println(i);
+                Vector<Integer> victor = new Vector<>();
+                victor.addElement(i);
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch(FileNotFoundException fnf) {
+            System.err.println("File: " + fileName + " not found.");
+        } catch (IOException io) {
+            System.err.println(io.toString());
         }
-//        catch and finally blocks  . . .
     }
+
+    public static void cat(File file) {
+        RandomAccessFile input = null;
+        String line = null;
+
+        try {
+            input = new RandomAccessFile(file, "r");
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+            return;
+        } catch(FileNotFoundException fnf) {
+            System.err.format("File: %s not found%n", file);
+        } catch(IOException e) {
+            System.err.println(e.toString());
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch(IOException io) {
+                }
+            }
+        }
+    }
+
 }
