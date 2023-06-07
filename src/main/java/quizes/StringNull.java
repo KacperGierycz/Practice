fed07a7fa869b6f2e3b19a25ae1fc371d551e082
Kacper <kacpergierycz@gmail.com>
2023-06-07 15:20:45 +0200

    quiz all day long

---
+++ src/main/java/quizes/StringNull.java
@@ -0,0 +1,21 @@
+package quizes;
+
+public class StringNull {
+
+    public static void main(String[] args) {
+
+        short[] z [] [];
+
+        String str = "null";
+        if (str == null){
+            System.out.println("null");
+        }
+
+        if(str.length() == 0){
+            System.out.println("0");
+        }
+        else{
+            System.out.println("sss");
+        }
+    }
+}
