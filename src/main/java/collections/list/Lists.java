9bac4f88d66111f7e5aa7633a963ed1008b06898
Kacper <kacpergierycz@gmail.com>
2023-06-27 15:12:19 +0200

    List check for equals method

---
+++ src/main/java/collections/list/Lists.java
@@ -0,0 +1,42 @@
+package collections.list;
+
+import collections.set.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lists {
+    List<Person> list1 = Arrays.asList(new Person("Wojtek", 0), new Person("Kacper", 0));
+
+    List<Person> list4 = Arrays.asList(new Person("Wojtek", 0), new Person("Kacper", 0));
+
+
+    List<Person> list3 = new ArrayList<Person>(list1);
+    List<Person> list2 = new ArrayList<Person>(list4);
+    List<Person> people;
+
+
+ public Lists(){
+    people = new ArrayList<>(list2);
+     System.out.println(people.equals(list2));
+    list2.addAll(list3);
+     System.out.println(people);
+     System.out.println(list2);
+     List<String> list = people.stream()
+             .map(Person::getName)
+             .collect(Collectors.toList());
+
+
+
+     System.out.println(list);
+
+}
+
+
+
+    public static void main(String[] args) {
+        Lists l = new Lists();
+    }
+}
