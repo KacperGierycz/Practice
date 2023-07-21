2283b3cd5860fbf2390f931bfd3e013ac02f0357
Kacper <kacpergierycz@gmail.com>
2023-07-21 14:36:02 +0200

    Peek and add element with peek during same stream

---
+++ src/main/java/aggregateOperations/parallelism/Interference.java
@@ -0,0 +1,49 @@
+package aggregateOperations.parallelism;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Interference {
+
+
+    public static void main(String[] args) {
+
+        //Use of peek
+        System.out.println("Start of peek use case presentation: ");
+        Stream.of("one", "two", "three", "four")
+                .filter(e -> e.length() > 3)
+                .peek(e -> System.out.println("Filtered value: " + e))
+                .map(String::toUpperCase)
+                .peek(e -> System.out.println("Mapped value: " + e))
+                .collect(Collectors.toList());
+
+        System.out.println("The end of peek use presentation.");
+
+        try {
+            List<String> listOfStrings =
+                    new ArrayList<>(Arrays.asList("one", "two"));
+
+            // This will fail as the peek operation will attempt to add the
+            // string "three" to the source after the terminal operation has
+            // commenced.
+
+            String concatenatedString = listOfStrings
+                    .stream()
+
+                    // Don't do this! Interference occurs here.
+                    .peek(s -> listOfStrings.add("three"))
+
+                    .reduce((a, b) -> a + " " + b)
+                    .get();
+
+            System.out.println("Concatenated string: " + concatenatedString);
+
+        } catch (Exception e) {
+            System.out.println("Exception caught: " + e.toString());
+        }
+
+    }
+}
