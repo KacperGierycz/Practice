0018335428b48e17f12a4219509bfed3a0965da6
Kacper <kacpergierycz@gmail.com>
2023-08-01 15:35:15 +0200

    Sorting with comparator

---
+++ src/main/java/collections/algorithms/Sort.java
@@ -0,0 +1,13 @@
+package collections.algorithms;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(args);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
