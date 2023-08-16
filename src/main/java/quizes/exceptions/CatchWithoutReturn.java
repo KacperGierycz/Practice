5b4de704abbd2e97e655fecccc4caabfdd5aff9a
Kacper <kacpergierycz@gmail.com>
2023-08-16 15:01:16 +0200

    no return in finally but increment still incrementing

---
+++ src/main/java/quizes/exceptions/CatchWithoutReturn.java
@@ -0,0 +1,35 @@
+package quizes.exceptions;
+
+public class CatchWithoutReturn {
+
+
+        static String anyMethod()
+        {
+            String s = "ONE";
+
+            try
+            {
+                s = s + "TWO";
+
+                return s;
+            }
+            catch (Exception e)
+            {
+                s = s + "THREE";
+
+                return s;
+            }
+            finally
+            {
+                s = s + "FOUR";
+                System.out.println(s);
+            }
+
+        }
+
+        public static void main(String[] args)
+        {
+            System.out.println(anyMethod());
+        }
+    }
+
