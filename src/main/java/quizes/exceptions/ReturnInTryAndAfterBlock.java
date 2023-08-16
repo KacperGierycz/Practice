dd54a7f2ceb1505c691344dd8e51c37efc2505e2
Kacper <kacpergierycz@gmail.com>
2023-08-16 15:33:50 +0200

    There should be return regardless that return is in try.

---
+++ src/main/java/quizes/exceptions/ReturnInTryAndAfterBlock.java
@@ -0,0 +1,33 @@
+package quizes.exceptions;
+
+public class ReturnInTryAndAfterBlock {
+
+    static int anyMethod()
+    {
+        int i = 1;
+
+        try
+        {
+            i = i + 1;
+
+            return i;
+        }
+        catch (Exception e)
+        {
+            i = i + 2;
+        }
+        finally
+        {
+            i = i + 3;
+        }
+
+        System.out.println(i);
+        return i;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(anyMethod());
+    }
+
+}
