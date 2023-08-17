781743cebc6a6d22ab1fa7566443291d99b127f3
Kacper <kacpergierycz@gmail.com>
2023-08-17 16:22:50 +0200

    Compare on Strings

---
+++ src/main/java/quizes/String/Compare.java
@@ -0,0 +1,11 @@
+package quizes.String;
+
+public class Compare {
+
+    public static void main(String[] args)
+    {
+        System.out.println("A".compareTo("B"));
+        System.out.println("B".compareTo("A"));
+    }
+
+}
