3ccd3ab01f46b1d0e5e8f13004e1e06c86aefc3f
Kacper <kacpergierycz@gmail.com>
2023-08-17 17:01:35 +0200

    IndexOf('a', fromIndex 5)

---
+++ src/main/java/quizes/String/IndexOf.java
@@ -0,0 +1,15 @@
+package quizes.String;
+
+public class IndexOf {
+
+    public static void main(String[] args) {
+
+        {
+            String str = "Java Concept Of The Day";
+
+            System.out.println(str.indexOf('a', 5));
+        }
+
+    }
+
+}
--- src/main/java/quizes/String/SmallExercises.java
+++ src/main/java/quizes/String/SmallExercises.java
@@ -15,6 +15,9 @@ public class SmallExercises {
         System.out.println(s3);
         System.out.println("ONE" + 1 + 2 + 3 + 4 + "FIVE");
         System.out.println(7.7 + 3.3 + "JAVA" + 3.3 + 7.7);
+
+
+
  //       System.out.println(null + 1 + "null");
    //     System.out.println("JAVAJ2EE".substring(2,5).substring(1).charAt(2));
         System.out.println(1000 + 2000 + 3000 + 4000 + "Java");
