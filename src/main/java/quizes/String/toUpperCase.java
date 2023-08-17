80bca703581b6ce0169c665dc8ba68d718399ea7
Kacper <kacpergierycz@gmail.com>
2023-08-17 15:22:34 +0200

    toUpperCase

---
+++ src/main/java/quizes/String/toUpperCase.java
@@ -0,0 +1,20 @@
+package quizes.String;
+
+public class toUpperCase {
+
+    public static void main(String[] args)
+    {
+        if("string".toUpperCase() == "STRING")
+        {
+            System.out.println(true );
+        }
+        else
+        {
+            System.out.println(false + "string".toUpperCase());
+        }
+
+        System.out.println("string".toUpperCase() == "STRING");
+
+    }
+
+}
