88b81c83d44a3b8886bee0fd22083bd98a1b2894
Kacper <kacpergierycz@gmail.com>
2023-08-17 15:14:00 +0200

    .strip

---
+++ src/main/java/quizes/String/Strip.java
@@ -0,0 +1,12 @@
+package quizes.String;
+
+public class Strip {
+
+    public static void main(String[] args)
+    {
+        String str = "  Java\tConcept\tOf\tThe\tDay     ".strip();
+
+        System.out.println(str);
+    }
+
+}
