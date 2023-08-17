b43025a5dc3ae757cd34d05d082092dff647691f
Kacper <kacpergierycz@gmail.com>
2023-08-17 16:08:10 +0200

    Intern next part I don't recognize why a.intern == b.intern == true

---
+++ src/main/java/quizes/String/InternMoreComplexAndToComplex.java
@@ -0,0 +1,23 @@
+package quizes.String;
+
+public class InternMoreComplexAndToComplex {
+
+    public static void main(String[] args)
+    {
+        String s1 = new String("JAVA");
+
+        String s2 = new String("JAVA");
+
+        System.out.println(s1 == s2);
+
+        System.out.println(s1.equals(s2));
+
+        System.out.println(s1 == s2.intern());
+
+        // why is that so?
+        System.out.println(s1.intern() == s2.intern());
+
+        System.out.println(s1.intern() == s2);
+    }
+
+        }
