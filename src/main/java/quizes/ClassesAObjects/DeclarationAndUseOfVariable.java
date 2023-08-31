71857252ddd2a858fb429669a1c4b64b7172d2bd
Kacper <kacpergierycz@gmail.com>
2023-08-31 15:23:32 +0200

    I got it you can assign values to variable when it is declared later in code but you can't use it.

---
+++ src/main/java/quizes/ClassesAObjects/DeclarationAndUseOfVariable.java
@@ -0,0 +1,19 @@
+package quizes.ClassesAObjects;
+
+public class DeclarationAndUseOfVariable {
+
+    {
+        i = 20;
+    }
+
+    {
+     //   System.out.println(i);
+    }
+
+    int i = 10;
+
+    {
+        System.out.println(i);
+    }
+
+}
