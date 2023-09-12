0025f62bf0fc1d2d016b5c198ff945d4f4ebf9ed
Kacper <kacpergierycz@gmail.com>
2023-09-12 15:56:05 +0200

    They call it ++ and -- can't be applied to constants but I would call it can't ++ or -- when variable is initialize.

---
+++ src/main/java/quizes/IncrementDecrement/IncDecCantBeApplyToConstants.java
@@ -0,0 +1,20 @@
+package quizes.IncrementDecrement;
+
+public class IncDecCantBeApplyToConstants {
+
+    public class IncrementDecrementQuiz
+    {
+        public static void main(String[] args)
+        {
+
+            // error variable expected
+         //   int a = 11++;
+
+            int a = 11;
+
+
+            System.out.println(a);
+        }
+    }
+
+}
