73ade54f6e3710e841c90a97f5518e306ba5fe17
Kacper <kacpergierycz@gmail.com>
2023-09-12 15:44:08 +0200

    can't nest -- nor ++

---
+++ src/main/java/quizes/IncrementDecrement/CantNestThose.java
@@ -0,0 +1,15 @@
+package quizes.IncrementDecrement;
+
+public class CantNestThose {
+
+
+        public static void main(String[] args)
+        {
+            int i = 11;
+
+            //can't nest those
+        //    int j = --(i++);
+        }
+
+
+}
