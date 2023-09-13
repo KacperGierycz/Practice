aa5422b069f27b5380acd2560bcb1f1b526c741a
Kacper <kacpergierycz@gmail.com>
2023-09-13 14:34:29 +0200

    while multiplication of elements their values cancel out each other to 1 value of equation is 0 if there is int type but 1 if double. This is due to partial values not evaluate to integer.

---
+++ src/main/java/quizes/IncrementDecrement/CrazyZero.java
@@ -0,0 +1,14 @@
+package quizes.IncrementDecrement;
+
+public class CrazyZero {
+
+
+        public static void main(String[] args)
+        {
+            double m=1010, n=1010;
+
+            System.out.println(m++ / ++n * n-- / --m);
+        }
+
+
+}
