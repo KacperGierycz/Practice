0020abb9c45d0e71bb465e5e93859667f456440a
Kacper <kacpergierycz@gmail.com>
2023-09-19 17:37:38 +0200

    infinite extension of classes by inner classes

---
+++ src/main/java/quizes/nestedClasses/InfiniteExtensions.java
@@ -0,0 +1,16 @@
+package quizes.nestedClasses;
+
+public class InfiniteExtensions {
+        class B extends InfiniteExtensions
+        {
+            class C extends B
+            {
+                class D extends C
+                {
+
+                }
+            }
+        }
+
+
+}
