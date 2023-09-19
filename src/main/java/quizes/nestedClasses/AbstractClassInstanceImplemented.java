ef92592a38bf5adb1ac6569a73eab79c2248adea
Kacper <kacpergierycz@gmail.com>
2023-09-19 16:22:09 +0200

    Instance of the abstract class

---
+++ src/main/java/quizes/nestedClasses/AbstractClassInstanceImplemented.java
@@ -0,0 +1,14 @@
+package quizes.nestedClasses;
+
+public class AbstractClassInstanceImplemented {
+
+    {
+        System.out.println(1);
+    }
+
+    static
+    {
+        System.out.println(2);
+    }
+}
+
---
+++ src/main/java/quizes/nestedClasses/MainClassAbstractClassInstanceImplemented.java
@@ -0,0 +1,8 @@
+package quizes.nestedClasses;
+
+public class MainClassAbstractClassInstanceImplemented {
+    public static void main(String[] args) {
+        AbstractClassInstanceImplemented a = new AbstractClassInstanceImplemented();
+    }
+
+}
