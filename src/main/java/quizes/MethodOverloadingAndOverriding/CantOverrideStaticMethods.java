cb08d11428027d44381674bc3554add52daa2030
Kacper <kacpergierycz@gmail.com>
2023-09-28 15:02:56 +0200

    Can't override static methods but compiler do not recognize it.

---
+++ src/main/java/quizes/MethodOverloadingAndOverriding/CantOverrideStaticMethods.java
@@ -0,0 +1,22 @@
+package quizes.MethodOverloadingAndOverriding;
+
+public class CantOverrideStaticMethods {
+
+    class X
+    {
+        static void methodOfX()
+        {
+            System.out.println("Class X");
+        }
+    }
+
+    class Y extends X
+    {
+       // @Override
+        static void methodOfX()
+        {
+            System.out.println("Class X");
+        }
+    }
+
+}
