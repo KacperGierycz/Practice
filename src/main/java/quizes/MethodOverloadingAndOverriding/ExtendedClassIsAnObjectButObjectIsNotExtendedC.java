c05f3c17050e1bbe3fa9591ea48b5d3be5dd6028
Kacper <kacpergierycz@gmail.com>
2023-09-28 14:57:18 +0200

    I found quite interesting thing a little ambiguous if you extend class and override the method if you create an instance of type super and instance of extended class you have working override method but if you have a hidden variable in extended class value is from super class.

---
+++ src/main/java/quizes/MethodOverloadingAndOverriding/ExtendedClassIsAnObjectButObjectIsNotExtendedC.java
@@ -0,0 +1,75 @@
+package quizes.MethodOverloadingAndOverriding;
+
+public class ExtendedClassIsAnObjectButObjectIsNotExtendedC {
+
+    class X
+    {
+        int i = 10;
+        void calculate(int a, int b)
+        {
+            System.out.println("Class X");
+        }
+    }
+
+    class Y extends X
+    {
+        int i = 20;
+        @Override
+        void calculate(int a, int b)
+        {
+            System.out.println("Class Y");
+        }
+    }
+
+    class Z extends Y
+    {
+        int i = 30;
+        @Override
+        void calculate(int a, int b)
+        {
+            System.out.println("Class Z");
+        }
+    }
+
+    public class MainClass
+    {
+        public static void main(String[] args)
+        {
+            X x = new ExtendedClassIsAnObjectButObjectIsNotExtendedC(). new Y();
+
+            x.calculate(10, 20);
+
+
+            Y y = (Y) x;
+
+            y.calculate(50, 100);
+
+
+            Z z = (Z) y;
+
+            z.calculate(100, 200);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        X x = new ExtendedClassIsAnObjectButObjectIsNotExtendedC(). new Y();
+        X xx = new ExtendedClassIsAnObjectButObjectIsNotExtendedC().new X();
+
+        xx.calculate(10,20);
+        x.calculate(10, 20);
+        System.out.println(x.i);
+        System.out.println(xx.i);
+        Y y = (Y) x;
+        Y yy = (Y) xx;
+
+        y.calculate(50, 100);
+        System.out.println(y.i);
+        Z z = (Z) y;
+
+        z.calculate(100, 200);
+        System.out.println(z.i);
+    }
+
+}
