268b17bc0ecdfcaf576876f398e1d0519486ef10
Kacper <kacpergierycz@gmail.com>
2023-09-29 15:56:09 +0200

    When method is being overridden, but it has no distinct arguments whole method signature is the same ques what.... super class method is used.

---
+++ src/main/java/quizes/MethodOverloadingAndOverriding/WhenThereIsNoArgumentsButMethodOverride.java
@@ -0,0 +1,38 @@
+package quizes.MethodOverloadingAndOverriding;
+
+public class WhenThereIsNoArgumentsButMethodOverride {
+
+    class A
+    {
+        static void methodOne()
+        {
+            System.out.println("AAA");
+        }
+    }
+
+    class B extends A
+    {
+        static void methodOne()
+        {
+            System.out.println("BBB");
+        }
+    }
+
+    public class MainClass
+    {
+        public static void main(String[] args)
+        {
+            A a = new WhenThereIsNoArgumentsButMethodOverride(). new B();
+
+            a.methodOne();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        A a = new WhenThereIsNoArgumentsButMethodOverride(). new B();
+
+        a.methodOne();
+    }
+
+}
