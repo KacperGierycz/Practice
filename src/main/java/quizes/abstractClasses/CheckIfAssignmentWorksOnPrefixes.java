3fac1672b0fad1eb4f3dccb34e063cf670eed767
Kacper <kacpergierycz@gmail.com>
2023-10-10 14:46:37 +0200

    assignment works on prefixes

---
+++ src/main/java/quizes/abstractClasses/CheckIfAssignmentWorksOnPrefixes.java
@@ -0,0 +1,14 @@
+package quizes.abstractClasses;
+
+public class CheckIfAssignmentWorksOnPrefixes {
+
+   static int i = 1;
+
+    public static void main(String[] args) {
+
+        i = ++i;
+        System.out.println(i);
+
+    }
+
+}
