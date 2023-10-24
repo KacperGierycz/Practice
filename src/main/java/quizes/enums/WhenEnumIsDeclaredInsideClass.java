72ddb7b300bfcc73b394132d2b6eb2bbca4d4361
Kacper <kacpergierycz@gmail.com>
2023-10-24 16:22:43 +0200

    There is no access to this class static block

---
+++ src/main/java/quizes/enums/WhenEnumIsDeclaredInsideClass.java
@@ -0,0 +1,44 @@
+package quizes.enums;
+
+public class WhenEnumIsDeclaredInsideClass {
+
+    class A
+    {
+        {
+            System.out.println(1);
+        }
+
+        static
+        {
+            System.out.println(2);
+        }
+
+        enum Enums
+        {
+            A;
+
+            {
+                System.out.println(3);
+            }
+
+            static
+            {
+                System.out.println(4);
+            }
+
+            class B
+            {
+                {
+                    System.out.println(5);
+                }
+            }
+        }
+    }
+
+        public static void main(String[] args)
+        {
+            System.out.println(A.Enums.A);
+        }
+
+
+}
