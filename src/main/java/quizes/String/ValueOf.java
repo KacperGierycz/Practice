14390b0762aa9dac9a2974db896f30097e3ecb32
Kacper <kacpergierycz@gmail.com>
2023-11-06 16:37:50 +0100

    Value.of converts value to the String.

---
+++ src/main/java/quizes/String/ValueOf.java
@@ -0,0 +1,11 @@
+package quizes.String;
+
+public class ValueOf {
+
+        public static void main(String[] args)
+        {
+            System.out.println(String.valueOf(10)+String.valueOf(1.1)+String.valueOf(true));
+        }
+
+
+}
