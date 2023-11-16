b4772dfcacd3926914607f46ea17e0c716d9439d
Kacper <kacpergierycz@gmail.com>
2023-11-16 14:42:57 +0100

    just 1 sec adjustment.

--- src/main/java/quizes/threads/WhyThereIsNoPrintOnInterrupt.java
+++ src/main/java/quizes/threads/WhyThereIsNoPrintOnInterrupt.java
@@ -11,7 +11,7 @@ public class WhyThereIsNoPrintOnInterrupt {
                 {
                     try {
                         System.out.println("going to sleep");
-                        Thread.sleep(3000);
+                        Thread.sleep(2000);
                         System.out.println("wake up ");
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
