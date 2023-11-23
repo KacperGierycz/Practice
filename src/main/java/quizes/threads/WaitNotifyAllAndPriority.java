3df9f87239adc9048a268ace1481001361c6fae2
Kacper <kacpergierycz@gmail.com>
2023-11-23 15:04:34 +0100

    There is some kind of blocking threads situation going on that I did't fix.

--- src/main/java/quizes/threads/WaitNotifyAllAndPriority.java
+++ src/main/java/quizes/threads/WaitNotifyAllAndPriority.java
@@ -6,52 +6,55 @@ public class WaitNotifyAllAndPriority {
         public static void main(String[] args)
         {
             AnyClass anyClass = new AnyClass();
-
+            System.out.println(1);
             Thread t1 = new Thread("Thread_1")
             {
                 @Override
                 public void run()
                 {
+                    System.out.println(2);
                     anyClass.methodOne();
                 }
             };
             t1.setPriority(Thread.MIN_PRIORITY);
             t1.start();
 
-            try
-            {
-                Thread.sleep(1000);
-            }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-
+//            try
+//            {
+//                Thread.sleep(1000);
+//            }
+//            catch (InterruptedException e)
+//            {
+//                e.printStackTrace();
+//            }
+            System.out.println(3);
             Thread t2 = new Thread("Thread_2")
             {
                 @Override
                 public void run()
                 {
+                    System.out.println(4);
                     anyClass.methodOne();
                 }
             };
             t2.setPriority(Thread.MAX_PRIORITY);
             t2.start();
 
-            try
-            {
-                Thread.sleep(1000);
-            }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
+//            try
+//            {
+//                Thread.sleep(1000);
+//            }
+//            catch (InterruptedException e)
+//            {
+//                e.printStackTrace();
+//            }
 
             Thread t3 = new Thread("Thread_3")
             {
                 @Override
                 public void run()
                 {
+                    System.out.println(5);
                     anyClass.methodTwo();
                 }
             };
