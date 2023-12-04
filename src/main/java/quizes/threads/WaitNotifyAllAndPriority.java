da17fa0de1a7ff775afec4f93a24843124606b93
Kacper <kacpergierycz@gmail.com>
2023-12-04 16:57:09 +0100

    I figure out how to make this method to work. But with this comes the true idea of sender receiver that I have to recall once more.

--- src/main/java/quizes/threads/WaitNotifyAllAndPriority.java
+++ src/main/java/quizes/threads/WaitNotifyAllAndPriority.java
@@ -16,17 +16,17 @@ public class WaitNotifyAllAndPriority {
                     anyClass.methodOne();
                 }
             };
-            t1.setPriority(Thread.MIN_PRIORITY);
+            t1.setPriority(3);
             t1.start();
 
-//            try
-//            {
-//                Thread.sleep(1000);
-//            }
-//            catch (InterruptedException e)
-//            {
-//                e.printStackTrace();
-//            }
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
             System.out.println(3);
             Thread t2 = new Thread("Thread_2")
             {
@@ -37,17 +37,17 @@ public class WaitNotifyAllAndPriority {
                     anyClass.methodOne();
                 }
             };
-            t2.setPriority(Thread.MAX_PRIORITY);
+            t2.setPriority(7);
             t2.start();
 
-//            try
-//            {
-//                Thread.sleep(1000);
-//            }
-//            catch (InterruptedException e)
-//            {
-//                e.printStackTrace();
-//            }
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
 
             Thread t3 = new Thread("Thread_3")
             {
