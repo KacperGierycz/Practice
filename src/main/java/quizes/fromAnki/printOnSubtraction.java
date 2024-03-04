99d09d359c30163933c2ea852f87408d7df628b3
Kacper <kacpergierycz@gmail.com>
2024-03-04 13:04:36 +0100

    I suspect that k-- is not changing class member variable k, because of scope and pass by value.

---
+++ src/main/java/quizes/fromAnki/printOnSubtraction.java
@@ -0,0 +1,20 @@
+package quizes.fromAnki;
+
+public class printOnSubtraction {
+
+        int k = 5;
+        public boolean checkIt(int k) {
+            return k-- > 0 ? true : false;
+        }
+        public void printThem() {
+            while (checkIt(k)) {
+                System.out.print(k--);
+            }
+        }
+        public static void main(String[] args) {
+            new printOnSubtraction().printThem();
+
+        }
+
+
+}
